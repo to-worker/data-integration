0aef83ebd1fa0eea2e85191012f7319151f76894
weifeng <wefieng@zqykj.com>
2018-06-01 11:15:38 +0800

    测试对象序列化传输

---
+++ src/main/java/com/zqykj/tldw/entity/User.java
@@ -0,0 +1,30 @@
+package com.zqykj.tldw.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author feng.wei
+ * @date 2018/6/1
+ */
+public class User {
+
+    private String id;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
