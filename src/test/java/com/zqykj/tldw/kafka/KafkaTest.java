76949a0ddcb6ba1bbf130e4c2feb161eb4ae4fd2
weifeng <weifeng@zqykj.com>
2018-06-06 17:14:04 +0800

    获取kafka topic的分区数

--- src/test/java/com/zqykj/tldw/kafka/KafkaTest.java
+++ src/test/java/com/zqykj/tldw/kafka/KafkaTest.java
@@ -2,13 +2,20 @@ package com.zqykj.tldw.kafka;
 
 import com.netposa.recognize.model.ProviderVehicleInfo;
 import com.zqykj.tldw.common.JobConstants;
+import com.zqykj.tldw.common.TldwConfig;
 import com.zqykj.tldw.service.Producer;
 import com.zqykj.tldw.util.BeanUtils;
+import org.apache.commons.configuration.Configuration;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
 import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Properties;
 import java.util.UUID;
 
 /**
@@ -60,4 +67,35 @@ public class KafkaTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void getPartitions(){
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(consumerProperties());
+        List<PartitionInfo> partitionInfos = consumer.partitionsFor("test");
+        System.out.println("partitionInfos: " + partitionInfos.size());
+        for (PartitionInfo pInfo: partitionInfos){
+            System.out.println("topic: " + pInfo.topic()  + " ,partitionNum: " + pInfo.partition());
+        }
+
+    }
+
+    private Properties consumerProperties() {
+        Configuration config = TldwConfig.config;
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getString("kafka.bootstrap.servers"));
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, config.getString("kafka.key.deserializer"));
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, config.getString("kafka.value.deserializer"));
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, config.getString("kafka.consume.group.id"));
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, config.getString("kafka.enable.auto.commit"));
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, config.getString("kafka.offset.reset"));
+        properties
+                .put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, config.getInt("kafka.max.partition.fetch.bytes"));
+        properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, config.getInt("kafka.max.poll.records"));
+        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        properties.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, "30000");
+
+        return properties;
+    }
+
 }
